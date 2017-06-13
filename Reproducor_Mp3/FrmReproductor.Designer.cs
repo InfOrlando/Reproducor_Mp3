f316a4ea3e57cad3d0a47ce7ea5b9926f2a67f9c
inforlandope <josep-115@hotmail.com>
2017-06-12 22:46:02 -0600

    cambios

--- Reproducor_Mp3/FrmReproductor.Designer.cs
+++ Reproducor_Mp3/FrmReproductor.Designer.cs
@@ -29,59 +29,86 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmReproductor));
-            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Reproductor = new AxWMPLib.AxWindowsMediaPlayer();
             this.btnAgregar = new DevExpress.XtraEditors.SimpleButton();
             this.listBoxCanciones = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDesplegarBiblioteca = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).BeginInit();
             this.SuspendLayout();
             // 
-            // axWindowsMediaPlayer1
+            // Reproductor
             // 
-            this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(152, 12);
-            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
-            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(378, 148);
-            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.Reproductor.Enabled = true;
+            this.Reproductor.Location = new System.Drawing.Point(122, 30);
+            this.Reproductor.Name = "Reproductor";
+            this.Reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Reproductor.OcxState")));
+            this.Reproductor.Size = new System.Drawing.Size(583, 50);
+            this.Reproductor.TabIndex = 0;
             // 
             // btnAgregar
             // 
             this.btnAgregar.Image = ((System.Drawing.Image)(resources.GetObject("btnAgregar.Image")));
-            this.btnAgregar.Location = new System.Drawing.Point(55, 200);
+            this.btnAgregar.Location = new System.Drawing.Point(142, 108);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(91, 80);
+            this.btnAgregar.Size = new System.Drawing.Size(152, 80);
             this.btnAgregar.TabIndex = 1;
-            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.Text = "Cargar Archivos";
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // listBoxCanciones
             // 
             this.listBoxCanciones.FormattingEnabled = true;
             this.listBoxCanciones.ItemHeight = 16;
-            this.listBoxCanciones.Location = new System.Drawing.Point(226, 216);
+            this.listBoxCanciones.Location = new System.Drawing.Point(122, 194);
             this.listBoxCanciones.Name = "listBoxCanciones";
-            this.listBoxCanciones.Size = new System.Drawing.Size(213, 212);
+            this.listBoxCanciones.Size = new System.Drawing.Size(215, 260);
             this.listBoxCanciones.TabIndex = 2;
+            this.listBoxCanciones.SelectedIndexChanged += new System.EventHandler(this.listBoxCanciones_SelectedIndexChanged);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(368, 108);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(131, 80);
+            this.simpleButton1.TabIndex = 3;
+            this.simpleButton1.Text = "Guardar a la \r\nBiblioteca";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // btnDesplegarBiblioteca
+            // 
+            this.btnDesplegarBiblioteca.Image = ((System.Drawing.Image)(resources.GetObject("btnDesplegarBiblioteca.Image")));
+            this.btnDesplegarBiblioteca.Location = new System.Drawing.Point(557, 108);
+            this.btnDesplegarBiblioteca.Name = "btnDesplegarBiblioteca";
+            this.btnDesplegarBiblioteca.Size = new System.Drawing.Size(131, 80);
+            this.btnDesplegarBiblioteca.TabIndex = 4;
+            this.btnDesplegarBiblioteca.Text = "Ver Biblioteca";
             // 
             // FrmReproductor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(935, 475);
+            this.Controls.Add(this.btnDesplegarBiblioteca);
+            this.Controls.Add(this.simpleButton1);
             this.Controls.Add(this.listBoxCanciones);
             this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.Reproductor);
             this.Name = "FrmReproductor";
             this.Text = "FrmReproductor";
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.Load += new System.EventHandler(this.FrmReproductor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer Reproductor;
         private DevExpress.XtraEditors.SimpleButton btnAgregar;
         private System.Windows.Forms.ListBox listBoxCanciones;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton btnDesplegarBiblioteca;
     }
 }
--- Reproducor_Mp3/FrmReproductor.cs
+++ Reproducor_Mp3/FrmReproductor.cs
@@ -8,12 +8,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using System.IO;
 
 namespace Reproducor_Mp3
 {
     public partial class FrmReproductor : DevExpress.XtraEditors.XtraForm
     {
-        bool Play = false;
+       // bool Play = false;
+
         string[] ArchivosMP3;
         string[] rutasArchivos;
         public FrmReproductor()
@@ -34,7 +36,43 @@ namespace Reproducor_Mp3
                 {
                     listBoxCanciones.Items.Add(ArchivosMP3);
                 }
+
+                Reproductor.URL = rutasArchivos[0];
+                listBoxCanciones.SelectedIndex = 0;
             }
+        }
+
+        private void listBoxCanciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Reproductor.URL = rutasArchivos[listBoxCanciones.SelectedIndex];
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            string NombreArchivo = "Musica.txt";
+
+            FileStream archivo = new FileStream(NombreArchivo, FileMode.Append, FileAccess.Write);
+            StreamWriter Escribir = new StreamWriter(archivo);
+
+
+           // StreamWriter sw = new StreamWriter("C:\\test.txt");
+
+            foreach (object lista in rutasArchivos)
+            {
+                Escribir.WriteLine(lista.ToString());
+            }
+
+            Escribir.Close();
+        }
+
+        private void FrmReproductor_Load(object sender, EventArgs e)
+        {
+          /*  StreamReader sr = new StreamReader(Convert.ToString("C:\\test.txt"));
+            while (sr.Peek() >= 0)
+            {
+                listBoxCanciones.Items.Add(Convert.ToString(sr.ReadLine()));
+            }
+            sr.Close();*/
         }
     }
 }
